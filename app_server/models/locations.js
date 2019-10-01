0e7b8690e17d237837d354faa069c516c39797da
nguyen.viet.hung <nguyen.viet.hung@framgia.com>
2019-10-01 18:43:48 +0700

    create model chapter 5

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,4 +1,33 @@
 const mongoose = require('mongoose');
+
+const openingTimeSchema = new mongoose.Schema({
+  days: {
+    type: String,
+    required: true
+  },
+  opening: String,
+  closing: String,
+  closed: {
+    type: Boolean,
+    required: true
+  }
+});
+
+const reviewSchema = new mongoose.Schema({
+  author: String,
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
+  reviewText: String,
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
 const locationSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -15,6 +44,9 @@ const locationSchema = new mongoose.Schema({
   coords: {
     type: {type: String},
     coordinates: [Number]
-  }
+  },
+  openingTimes: [openingTimeSchema],
+  reviews: [reviewSchema]
 });
-locationSchema.index({coords: '2dsphere'})
+locationSchema.index({coords: '2dsphere'});
+mongoose.model('Location', locationSchema);
