3af677e2c62dcbe31f11d861f7eca28b3a17fafa
Nguyen Viet Hung <hunguyen1702@gmail.com>
2019-10-07 23:55:39 +0700

    Chapter 6 /locations api

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,9 +1,49 @@
 const Loc = require('../models/locations');
 
-const locationsListByDistance = (req, res) => {
-  res
-    .status(200)
-    .json({status: "success"});
+const locationsListByDistance = async (req, res) => {
+  const lng = parseFloat(req.query.lng);
+  const lat = parseFloat(req.query.lat);
+  const maxDistance = parseInt(req.query.distance);
+
+  if (!lng || !lat || !maxDistance) {
+    return res
+      .status(404)
+      .json({
+        "message": "distance, lng and lat query parameters are required"
+      });
+  }
+
+  const near = {
+    type: 'Point',
+    coordinates: [lng, lat]
+  };
+  const geoOptions = {
+    distanceField: "distance.calculated",
+    spherical: true,
+    maxDistance,
+    limit: 10
+  };
+  try {
+    const results = await Loc.aggregate([{
+      $geoNear: {
+        near,
+        ...geoOptions
+      }
+    }]);
+    const locations = results.map(result => {
+      return {
+        id: result._id,
+        name: result.name,
+        address: result.address,
+        rating: result.rating,
+        facilities: result.facilities,
+        distance: `${result.distance.calculated.toFixed()}m`
+      }
+    });
+    return res.status(200).json(locations);
+  } catch (err) {
+    return res.status(404).json(err);
+  }
 };
 
 const locationsCreate = (req, res) => {
@@ -14,10 +54,20 @@ const locationsCreate = (req, res) => {
 
 const locationsReadOne = (req, res) => {
   Loc.findById(req.params.locationId)
-    .exec(function (err, location) {
-      res.status(200)
+    .exec((err, location) => {
+      if (!location) {
+        return res
+          .status(404)
+          .json({message: "Location not found"});
+      } else if (err) {
+        return res
+          .status(404)
+          .json({message: err});
+      }
+      res
+        .status(200)
         .json(location);
-    })
+    });
 };
 
 const locationsUpdateOne = (req, res) => {
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,9 +1,49 @@
+const Loc = require('../models/locations');
+
 const reviewsCreate = (req, res) => {
 
 };
 
 const reviewsReadOne = (req, res) => {
-
+  Loc
+    .findById(req.params.locationId)
+    .select('name reviews')
+    .exec((err, location) => {
+      if (!location) {
+        return res
+          .status(404)
+          .json({message: 'Location not found'});
+      } else if (err) {
+        return res
+          .status(404)
+          .json(err);
+      }
+      if (location.reviews && location.reviews.length > 0) {
+        const review = location.reviews.id(req.params.reviewId);
+        if (!review) {
+          return res
+          .status(404)
+          .json({message: 'Review not found'});
+        } else {
+          let response = {
+            location: {
+              name: location.name,
+              id: req.params.locationId
+            },
+            review
+          };
+          return res
+            .status(200)
+            .json(response);
+        }
+      } else {
+        return res
+          .status(404)
+          .json({
+            message: 'No reviews found'
+          });
+      }
+    });
 };
 
 const reviewsUpdateOne = (req, res) => {
